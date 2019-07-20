7414c9b92143b9d61df3f6cafba613c4515e02e2
makocchi-git <makocchi@gmail.com>
2019-07-21 03:37:06 +0900

    add showks-form

--- installer/hack/showks.sh
+++ installer/hack/showks.sh
@@ -22,8 +22,16 @@ KEYCLOAK_PASSWORD=$(/bin/echo -n ${KEYCLOAK_PASSWORD} | base64)
 KEYCLOAK_USERNAME=$(/bin/echo -n ${KEYCLOAK_USERNAME} | base64)
 KEYCLOAK_REALM=$(/bin/echo -n ${KEYCLOAK_REALM} | base64)
 DOMAIN=$(/bin/echo -n ${DOMAIN} | base64)
+WEBHOOK_TOKEN=$(/bin/echo -n ${WEBHOOK_TOKEN} | base64)
+REGISTRY_USERNAME=$(/bin/echo -n ${REGISTRY_USERNAME} | base64)
+REGISTRY_PASSWORD=$(/bin/echo -n ${REGISTRY_PASSWORD} | base64)
+REGISTRY_URL=$(/bin/echo -n ${REGISTRY_URL} | base64)
+# use GITHUB_TOKEN
+GITHUB_ACCESS_TOKEN=$(/bin/echo -n ${GITHUB_TOKEN} | base64)
+GITHUB_ORG=$(/bin/echo -n ${GITHUB_ORG} | base64)
+GITHUB_PRIVATE_KEY=$(cat ./cert/github.key | base64)
 
-for f in $(ls ./hack/manifests/showks/)
+for f in $(ls ./hack/manifests/showks/ | grep -v ingress.yaml)
 do
     sed -e "
         /__CONCOURSECI_PASSWORD__/ s/__CONCOURSECI_PASSWORD__/${CONCOURSECI_PASSWORD}/g
@@ -32,11 +40,24 @@ do
         /__CONCOURSECI_USERNAME__/ s/__CONCOURSECI_USERNAME__/${CONCOURSECI_USERNAME}/g
         /__CONCOURSECI_TARGET/     s/__CONCOURSECI_TARGET__/${CONCOURSECI_TARGET}/g
         /__GITHUB_TOKEN__/         s/__GITHUB_TOKEN__/${GITHUB_TOKEN}/g
+        /__GITHUB_ACCESS_TOKEN__/  s/__GITHUB_ACCESS_TOKEN__/${GITHUB_ACCESS_TOKEN}/g
+        /__GITHUB_PRIVATE_KEY__/   s/__GITHUB_PRIVATE_KEY__/${GITHUB_PRIVATE_KEY}/g
+        /__GITHUB_ORG__/           s/__GITHUB_ORG__/${GITHUB_ORG}/g
         /__KEYCLOAK_BASE_PATH__/   s/__KEYCLOAK_BASE_PATH__/${KEYCLOAK_BASE_PATH}/g
         /__KEYCLOAK_PASSWORD__/    s/__KEYCLOAK_PASSWORD__/${KEYCLOAK_PASSWORD}/g
         /__KEYCLOAK_USERNAME__/    s/__KEYCLOAK_USERNAME__/${KEYCLOAK_USERNAME}/g
         /__KEYCLOAK_REALM__/       s/__KEYCLOAK_REALM__/${KEYCLOAK_REALM}/g
         /__DOMAIN__/               s/__DOMAIN__/${DOMAIN}/g
+        /__WEBHOOK_TOKEN__/        s/__WEBHOOK_TOKEN__/${WEBHOOK_TOKEN}/g
+        /__REGISTRY_USERNAME__/    s/__REGISTRY_USERNAME__/${REGISTRY_USERNAME}/g
+        /__REGISTRY_PASSWORD__/    s/__REGISTRY_PASSWORD__/${REGISTRY_PASSWORD}/g
+        /__REGISTRY_URL__/         s/__REGISTRY_URL__/${REGISTRY_URL}/g
     " ./hack/manifests/showks/${f} > ./staging/showks/${f}
 done
 
+# ingress
+mkdir -p ./staging/ingress/cmn
+
+sed -e "
+    /__DOMAIN__/ s/__DOMAIN__/cmn.${DOMAIN}/g
+" ./hack/manifests/showks/ingress.yaml > ./staging/ingress/cmn/showks.yaml
