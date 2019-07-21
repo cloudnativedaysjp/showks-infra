c1fdf1629455118f81e261bb9062aff7af7b4088
makocchi-git <makocchi@gmail.com>
2019-07-22 00:07:36 +0900

    support prometheus ingress

--- installer/hack/prometheus.sh
+++ installer/hack/prometheus.sh
@@ -4,5 +4,19 @@ set -a
 
 mkdir -p ./staging/prometheus
 
-cp ./hack/manifests/prometheus/* ./staging/prometheus/.
+for f in $(ls ./hack/manifests/prometheus/ | grep -v ingress.yaml)
+do
+    cp ./hack/manifests/prometheus/${f} ./staging/prometheus/.
+done
 
+# ingress for grafana
+mkdir -p staging/ingress/prod/
+mkdir -p staging/ingress/stg/
+
+sed -e "
+    /__DOMAIN__/ s/__DOMAIN__/${PRD_DOMAIN}/g
+" ./hack/manifests/prometheus/ingress.yaml > ./staging/ingress/prod/prometheus.yaml
+
+sed -e "
+    /__DOMAIN__/ s/__DOMAIN__/${STG_DOMAIN}/g
+" ./hack/manifests/prometheus/ingress.yaml > ./staging/ingress/stg/prometheus.yaml
